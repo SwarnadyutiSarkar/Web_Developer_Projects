ee54bf0a23e14e09617d7d876edb3145b06c4293
Swarnadyuti Sarkar <166729648+SwarnadyutiSarkar@users.noreply.github.com>
2024-06-11 20:34:41 +0530

    ill upload the asset file later

---
+++ doll-dress-up-game/script.js
@@ -0,0 +1,9 @@
+function changeDress(dress) {
+    const dressImg = document.getElementById('dress');
+    dressImg.src = `assets/${dress}`;
+}
+
+function changeHat(hat) {
+    const hatImg = document.getElementById('hat');
+    hatImg.src = `assets/${hat}`;
+}
